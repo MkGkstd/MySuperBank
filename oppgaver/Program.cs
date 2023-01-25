73aca50481baea6fd7131363073171c1b2c98cfb
MkGkstd <Ahmed.Mohammed@gokstadakademiet.no>
2023-01-25 20:47:24 +0100

    Jeg la til tekst

---
+++ oppgaver/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Maki", 10000);
+            Console.WriteLine($"account {account.Number} was created for{account.Owner} with {account.Balance}.");
+            Console.WriteLine($"test..,");
+
+        }
+
+
+    }
+}
